2d9390b1fe86a995602541c38ead5cfa6b1d062c
Pelhumiesticks <babatundejohnoluwadamilare@gmail.com>
2023-05-21 22:17:19 -0700

    Hot fix

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,19 +6,19 @@
  */
 int main(void)
 {
-    char num;
+char num;
 
-    for (num = '0'; num <= '9'; num++)
-    {
-        putchar(num);
-    }
+for (num = '0'; num <= '9'; num++)
+{
+putchar(num);
+}
 
-    for (num = 'a'; num <= 'f'; num++)
-    {
-        putchar(num);
-    }
+for (num = 'a'; num <= 'f'; num++)
+{
+putchar(num);
+}
 
-    putchar('\n');
+putchar('\n');
 
-    return 0;
+return (0);
 }
