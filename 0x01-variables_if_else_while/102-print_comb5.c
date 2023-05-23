1823ef522249127b4efbc38b7f57edd9b43c09e2
Pelhumiesticks <babatundejohnoluwadamilare@gmail.com>
2023-05-22 21:14:14 -0700

    Hot fix for 102 print comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,7 +10,7 @@ int num1, num2;
 
 for (num1 = 0; num1 <= 99; num1++)
 {
-for (num2 = 0; num2 <= 99; num2++)
+for (num2 = num1 + 1; num2 <= 99; num2++)
 {
 if (num1 < 10)
 {
@@ -26,7 +26,7 @@ putchar('0');
 }
 printf("%d", num2);
 
-if (num1 != 99 || num2 != 99)
+if (num1 != 98 || num2 != 99)
 {
 putchar(',');
 putchar(' ');
