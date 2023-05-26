4143c79c6e3e6c15d9b63f9282b86b32c73ca28f
Pelhumiesticks <babatundejohnoluwadamilare@gmail.com>
2023-05-26 07:05:39 -0700

    Hot fix

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - check for upper case characters
+ * Return: 0 or 1
+ */
+
 int _isupper(int c);
 {
 if (c >= 'A' && c <= 'Z')
@@ -11,20 +16,4 @@ else
 {
 return (0);
 }
-}
-
-/**
- * main - check for upper case characters
- * Return: 0 if otherwise
- */
-
-int main(void)
-{
-char c;
-
-c = 'A';
-printf("%c: %d\n", c, _isupper(c));
-c = 'a';
-printf("%c: %d\n", c, _isupper(c));
-return (0);
 }
