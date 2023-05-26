3144d7f6ed5f8ee9974e8b5393b891dc17b16874
Pelhumiesticks <babatundejohnoluwadamilare@gmail.com>
2023-05-26 15:15:42 -0700

    Various commits

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_square - prints a square line in the terminal
+ * @n: number of times the character '#' should be printed
+ * Return: void
+ */
+
+void print_square(int size)
+{
+int i;
+if (size <= 0)
+{
+_putchar('\n');
+return;
+for (i = 0; i < n; i++)
+{
+_putchar(' ');
+}
+_putchar('\n');
+}
+
